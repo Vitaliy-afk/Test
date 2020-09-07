75096e8b590118c2896669168abc92ffe8d9d50b
Vitaliy <53347040+Vitaliy-afk@users.noreply.github.com>
2020-09-07 04:50:01 +0300

    first commit

---
+++ assets/js/menu.js
@@ -0,0 +1,40 @@
+jQuery( document ).ready(function() {
+	initSlickSlider();
+});
+
+
+function initBurgerMenu() {
+	jQuery('.burger').on('click', function(e){
+		e.preventDefault()
+		jQuery(this).toggleClass('open');
+		jQuery('body').toggleClass('burger-active');
+	});
+}
+
+$('.count').each(function () {
+	$(this).prop('Counter',0).animate({
+		Counter: $(this).text()
+	}, {
+		duration: 4000,
+		easing: 'swing',
+		step:function(now){
+			$(this).text(Math.ceil(now));
+		}
+	});
+});
+
+function initSlickSlider() {
+	jQuery('.slider-block').slick({
+	infinite: true,
+	speed: 800,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	arrows: true,
+	easing: 'ease',
+	dots: true,
+	arrows: true
+})};
+
+
+
+
